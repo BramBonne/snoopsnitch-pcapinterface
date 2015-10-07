e9bbea1eb188ab549d25db1e6272536583308c91
Bram Bonné <bram.bonne@gmail.com>
2015-10-07 17:02:46 +0200

    Improved buildrun script to handle errors.

--- SnoopSnitch/buildrun.sh
+++ SnoopSnitch/buildrun.sh
@@ -1,3 +1,2 @@
-ant debug;
-adb shell am start -n de.srlabs.snoopsnitch/de.srlabs.snoopsnitch.StartupActivity;
-adb logcat -s msd-service;
+rm bin/*.apk
+ant debug && adb install -r bin/SnoopSnitch-debug.apk && adb shell am start -n de.srlabs.snoopsnitch/de.srlabs.snoopsnitch.StartupActivity && adb logcat -s msd-service
