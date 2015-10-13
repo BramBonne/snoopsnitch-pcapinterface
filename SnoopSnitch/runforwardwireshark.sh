e8f60e48463fd62c1dc91aa4b36a41d41b190cb1
Bram Bonné <bram.bonne@gmail.com>
2015-10-13 13:42:17 +0200

    Require bash for input streams. Wait for snoopsnitch to start before capturing.

--- SnoopSnitch/runforwardwireshark.sh
+++ SnoopSnitch/runforwardwireshark.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 adb shell am force-stop de.srlabs.snoopsnitch
 adb forward tcp:9742 tcp:9742
 adb shell am start -n de.srlabs.snoopsnitch/de.srlabs.snoopsnitch.StartupActivity
-wireshark -k -i <(netcat 127.0.0.1 9742) 
+sleep 3
+wireshark -k -i <(netcat 127.0.0.1 9742)
+adb shell am force-stop de.srlabs.snoopsnitch
