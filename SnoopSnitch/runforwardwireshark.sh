1044a96992fce1152163dc904d4850fd5f229b07
Bram Bonné <bram.bonne@gmail.com>
2015-10-13 11:29:44 +0200

    Added run script that allows Wireshark monitoring over USB.

---
+++ SnoopSnitch/runforwardwireshark.sh
@@ -0,0 +1,4 @@
+adb shell am force-stop de.srlabs.snoopsnitch
+adb forward tcp:9742 tcp:9742
+adb shell am start -n de.srlabs.snoopsnitch/de.srlabs.snoopsnitch.StartupActivity
+wireshark -k -i <(netcat 127.0.0.1 9742) 
